b5ce73be32b6316ca66cf70ae30206dadabf2606
ChristopherDelgado <Christopher.delgado.754@my.csun.edu>
2021-03-15 18:30:16 -0700

    Added Comments to explain each different method

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
+	/* A recursive approach to finding the nth fibonacci, this approach has two
+		main problems with it, First we are loading the call stack repeatedly with
+		new recursiveFibonacci calls which can result in a StackOverflow error,
+		Second we will be solving the same problem multiple times for example to
+		find the 5th fibonacci number we have to solve the 3rd fibonacci number twice
+		this doesnt seem that bad but imagine if we want the 100th fibonacci number */
 	private int recursiveFibonacci(int n)
 	{
 		if(n < 2)
@@ -9,6 +15,12 @@ public class Fibonacci {
 		return recursiveFibonacci(n-1) + recursiveFibonacci(n-2);
 	}
 
+	/* A recursive apporach that utilizes the memoization technique, here we still
+	make recursive calls but we store the values in an array so that if we later
+	need to solve a fibonacci number that we already solved before we can just use
+	the value we found before from our 'memo' array, this approach fixes the original
+	approaches problem with solving the same fibonacci numbers mutliple times but
+	is still susceptible to StackOverflow if the 'n' input is still high enough */
 	private int memoizeFibonacci(int n, int[] memo)
 	{
 		if(n < 2)
@@ -19,6 +31,10 @@ public class Fibonacci {
 		return memo[n];
 	}
 
+	/* An iterative approach that solves the problems with the two previous approaches,
+	it cannot cause a StackOverflow error and will not solve the same fibonacci numbers
+	more than once, what we do is find every fiboncacci number up until n, these are stored
+	in the 'memoizeArr' array so when we finish we return memoizeArr[n] */
 	private int bottomUpFibonacci(int n)
 	{
 		int[] memoizeArr = new int[n+1];
@@ -29,6 +45,7 @@ public class Fibonacci {
 		return memoizeArr[n];
 	}
 
+	/* A basic function used to ask the user for an integer with the provided message */
 	private static int ScannerResult(String message)
 	{
 		int result = 0;
@@ -44,6 +61,8 @@ public class Fibonacci {
 		}
 		return result;
 	}
+
+	/* This function starts the program */
 	public static void Start()
 	{
 		int fibOf = 0;
